ba50d684e4ff04623f4acf39cb11e72ac73be1c1
L4louve <93912472+L4louve@users.noreply.github.com>
2022-10-08 05:45:07 +0200

    Menu

---
+++ Assets/Scripts/Menu/playbutton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class playbutton : MonoBehaviour {
+
+public string scenename;
+public bool  showGUI;
+public AudioClip sounds;
+public AudioSource sound;
+
+void Awake (){
+}
+
+void  Update (){
+	if(showGUI == true){
+		LOAD_SCENE();
+	}
+}
+
+void  LOAD_SCENE (){
+	SceneManager.LoadScene(scenename);
+}
+
+void  OnTriggerEnter ( Collider hit  ){
+	if(hit.gameObject.tag == "Player"){
+		showGUI = true;
+	}
+}
+
+void  OnTriggerExit ( Collider hit  ){
+	if(hit.gameObject.tag == "Player"){
+		showGUI = false;
+	}
+}
+}
---
+++ Assets/Scripts/Menu/save_systeme.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public static class save_systeme
+{
+
+	public static void save_parametre(loading loading){
+
+		BinaryFormatter formatter = new BinaryFormatter();
+
+		string path = Application.persistentDataPath + "/paramtre.fun";
+		FileStream stream = new FileStream(path, FileMode.Create);
+
+		parametre_data data = new parametre_data(loading);
+
+		formatter.Serialize(stream, data);
+		stream.Close();
+
+
+	}
+
+	public static parametre_data Load_parametre(){
+		string path = Application.persistentDataPath + "/paramtre.fun";
+
+		if(File.Exists(path)){
+
+			BinaryFormatter formatter = new BinaryFormatter();
+			FileStream stream = new FileStream(path, FileMode.Open);
+			parametre_data data = formatter.Deserialize(stream) as parametre_data;
+			stream.Close();
+
+			return data;
+
+		}else{
+			Debug.LogError("Save file not found in" + path);
+			return null;
+		}
+
+	}
+
+}
