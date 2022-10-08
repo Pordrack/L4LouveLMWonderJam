ff3e912a9896b6adbe8cb796e95704e3e6fbe9ff
Kabourlix <hdamaia@outlook.fr>
2022-10-08 15:14:04 -0400

    Working on animations of UI (done)

--- Assets/Scripts/UI/CustomText.cs
+++ Assets/Scripts/UI/CustomText.cs
@@ -1,6 +1,5 @@
-using System;
-using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 namespace UI
@@ -8,15 +7,26 @@ namespace UI
     public class CustomText : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
     {
         private Animator _animator;
+
+        [SerializeField] private UnityEvent onClick;
+        
         
         void Start()
         {
             _animator = GetComponent<Animator>();
+            OnAnimationEnd.OnAnimationEndEvent += AnimationEnded;
+        }
+
+        private void AnimationEnded(Animator obj)
+        {
+            if (!obj.Equals(_animator)) return;
+            onClick?.Invoke();
         }
 
         public void OnPointerClick(PointerEventData eventData)
         {
-            _animator.SetTrigger("Click");
+            if (eventData.button != PointerEventData.InputButton.Left) return;
+            _animator.SetTrigger("Selected");
         }
 
         public void OnPointerEnter(PointerEventData eventData)
