3363b87713091680cc8b220d7f74c74fcff3fb46
Kabourlix <hdamaia@outlook.fr>
2022-10-09 12:37:18 -0400

    Worked on the decision process

---
+++ Assets/Scripts/IA/GlitchedDecision.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace IA
+{
+    public class GlitchedDecision : DecisionMaker
+    
+    {
+        public GlitchedDecision(OtherNavBehavior nav, Brain brain, int moveAmount, Transform transform) : base(nav, brain, moveAmount, transform)
+        {
+        }
+
+        public override void Decide()
+        {
+            //Gello there
+        }
+
+        public override void Die()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Scripts/IA/NormalDecision.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace IA
+{
+    public class NormalDecision : DecisionMaker
+    {
+        
+        public NormalDecision(OtherNavBehavior nav, Brain brain, int moveAmount, Transform transform) : base(nav, brain, moveAmount, transform)
+        {
+        }
+
+        public override void Decide()
+        {
+            for(var i=0; i<MoveAmount; i++)
+            {
+                var pos = Tf.position; //Get Cerf position
+                var surroundings = Brain.GetAvailableSurrounding(pos);
+
+                var rand = Random.Range(0, (int)surroundings.Count);
+                Nav.PerformMove(surroundings[rand]);
+            }
+        }
+
+        public override void Die()
+        {
+            Debug.Log("He's dead Jim.");
+        }
+
+    }
+}
